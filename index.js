e3d3d222645ebc10c1fd25daee0a0c4e0b55d6cd
SaylaRdota <michelyaneslopez@gmail.com>
2022-05-11 17:06:24 -0400

    bgjhf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const { exec } = require("child_process");
+const { log } = require("console");
 
 console.log('Instalando dependencias...');
 console.log('Este proceso puede tardar');
@@ -12,17 +13,25 @@ exec("npm i", (err, stdout, stderr) => {
     return console.log("Hubo un error ejecucion: ", stderr);
   }
   console.log(`Todo ok: ${stdout}`);
-  const express = require("express");
-  const app = express();
-
-  app.use(express.static("dist"));
-  app.use("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./dist/zona-caliente/index.html"));
-  });
-
-  let port = 4443;
-  var server = app.listen(port, function () {
-    //muestro algo en la consola para saber si arranco bien el proycto
-    console.log("Servidor web escuchando en el puerto " + port);
-  });
+  
+  exec("npm run build", (err2, stdout2, stderr2) => {
+    if (err2) console.log("Hubo un error: ", err2);
+    if (stderr2) {
+      return console.log("Hubo un error ejecucion: ", stderr2);
+    }
+    console.log('Build completado');
+    const express = require("express");
+    const app = express();
+  
+    app.use(express.static("dist"));
+    app.use("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./dist/zona-caliente/index.html"));
+    });
+  
+    let port = 4443;
+    var server = app.listen(port, function () {
+      //muestro algo en la consola para saber si arranco bien el proycto
+      console.log("Servidor web escuchando en el puerto " + port);
+    });
+  })
 });
